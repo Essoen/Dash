96af014865e8a704df1f1b5c23759843bc748e9b
Stein-Otto Svorstøl <steinotto.svorstol@gmail.com>
2013-10-11 17:09:09 +0200

    Looks nicer.

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 var host = "http://10.0.0.9:8081";
 var apiKey = "46abdc48319c66c67962883b06ec7f74";
-function getNew(){ 
+function getHistory(){ 
 	var log = $("#log"); 
 	log.append("Fetching data, hold on...");
 	var sendUrl = host + "/api/" + apiKey + "/?cmd=history&type=downloaded&limit=25&jsonp=history&callback=?";
@@ -12,10 +12,23 @@ function getNew(){
 		success: function(data){
 			if (data.result == "success"){
 				log.append("We did it!"); 
-				log.append(data);
+				reprHistory(data); 
 			}else{
 				log.append("Something is wrong.");
 			}
 		}
 	})
+}
+
+function reprHistory(data){
+	var episodes = data.data; 
+	
+}
+
+function getUpcoming(){
+
+}
+
+function reprUpcoming(){
+
 }

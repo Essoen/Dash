1a7e48996227f3ad0b60e72962f26ecff7d8ff54
Stein-Otto Svorstøl <steinotto.svorstol@gmail.com>
2013-10-11 16:38:33 +0200

    Initial commit. Gets data from Sickbeard. Todo: represent data.

---
+++ js.js
@@ -0,0 +1,21 @@
+var host = "http://10.0.0.9:8081";
+var apiKey = "46abdc48319c66c67962883b06ec7f74";
+function getNew(){ 
+	var log = $("#log"); 
+	log.append("Fetching data, hold on...");
+	var sendUrl = host + "/api/" + apiKey + "/?cmd=history&type=downloaded&limit=25&jsonp=history&callback=?";
+	$.ajax({
+		type: "POST",
+		url: host + "/api/" + apiKey + "/?cmd=history&type=downloaded&limit=25&jsonp=hist",
+		data: String,
+		dataType: "jsonp", 
+		success: function(data){
+			if (data.result == "success"){
+				log.append("We did it!"); 
+				log.append(data);
+			}else{
+				log.append("Something is wrong.");
+			}
+		}
+	})
+}

1477119fa4eb44bc2947507294857c3b16b3ab73
Stein-Otto Svorstøl <steinotto.svorstol@gmail.com>
2013-10-11 17:51:04 +0200

    Now also represents upcoming.

--- js.js
+++ js.js
@@ -1,9 +1,18 @@
 var host = "http://10.0.0.9:8081";
 var apiKey = "46abdc48319c66c67962883b06ec7f74";
+
+function refresh(){
+	clearFields(); 
+	getHistory();
+	getUpcoming(); 
+}
+function clearFields(){
+	$("#log").html("");
+	$("#history").html("");
+	$("#upcoming").html("");
+}
 function getHistory(){ 
-	var log = $("#log"); 
-	log.append("Fetching data, hold on...");
-	var sendUrl = host + "/api/" + apiKey + "/?cmd=history&type=downloaded&limit=25&jsonp=history&callback=?";
+	var log = $("#log");
 	$.ajax({
 		type: "POST",
 		url: host + "/api/" + apiKey + "/?cmd=history&type=downloaded&limit=25&jsonp=hist",
@@ -11,8 +20,7 @@ function getHistory(){
 		dataType: "jsonp", 
 		success: function(data){
 			if (data.result == "success"){
-				log.append("We did it!"); 
-				reprHistory(data); 
+				reprHist(data); 
 			}else{
 				log.append("Something is wrong.");
 			}
@@ -20,19 +28,39 @@ function getHistory(){
 	})
 }
 
-function reprHistory(data){
+function getUpcoming(){
+	var log = $("#log");
+	$.ajax({
+		type: "POST",
+		url: host + "/api/" + apiKey + "/api/46abdc48319c66c67962883b06ec7f74/?cmd=future&sort=date&jsonp=up",
+		data: String,
+		dataType: "jsonp", 
+		success: function(data){
+			if (data.result == "success"){
+				reprUpcoming(data); 
+			}else{
+				log.append("Something is wrong.");
+			}
+		}
+	})
+}
+function reprHist(data){
+	var loc = $("#history")
 	var episodes = data.data; 
-	var loc = $("#history");
 	for (var i = 0; i < episodes.length; i++){
 		var ep = episodes[i];
 		loc.append(ep.show_name + " - " + ep.season + "x" + ep.episode +"<br>"); 
 	}
 }
 
-function getUpcoming(){
-
-}
-
-function reprUpcoming(){
-
+function reprUpcoming(data){
+	var loc = $("#upcoming"); 
+	var eps  = [data.data.today, data.data.soon, data.data.later];
+	for (var time = 0; time < eps.length; time++){
+		var episodes = eps[time]; 
+		for (var i = 0; i < episodes.length; i++){
+			var ep = episodes[i];
+			loc.append(ep.show_name+ " - " + ep.airdate + " - " + ep.season + "x" + ep.episode +"<br>"); 
+		}
+	}
 }

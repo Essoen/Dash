26b0b018179ba4f092090d11fd955b1f1419ac40
Stein-Otto Svorstøl <steinotto.svorstol@gmail.com>
2013-10-19 15:02:37 +0200

    Now only adds running shows to the "Downloaded episodes" list

--- js/js.js
+++ js/js.js
@@ -50,22 +50,6 @@ function getUpcoming(){ // Cotacts SickBeard and gets the upcoming episodes.
 	});
 }
 
-function presentHistory(data){ // Presents the history-data
-	var loc = $("#history");
-	var episodes = data.data; 
-	var addedEpisodes = new Array(); // Holds the added episodes, so we can check if a download is just a new version.
-	for (var i = 0; i < episodes.length; i++){
-		var ep = episodes[i];
-		if ( addedEpisodes.indexOf(ep.show_name+ep.season+ep.episode) != -1) {
-			continue; 
-		}
-		var btn = "<a href='"+"'<span class='glyphicon glyphicon-play'></span>"; // Needs padding.
-		loc.append( btn + "<a onClick='checkOff()' >"+ ep.show_name + " - " + ep.season + " x " + ep.episode +"</a><br>"); 
-		loc.append(getFilePath(ep)); 
-		addedEpisodes.push(ep.show_name+ep.season+ep.episode);
-	}
-}
-
 function presentUpcoming(data){ // Presents the upcoming-data
 	var loc = $("#upcoming"); 
 	var eps  = [data.data.today, data.data.soon, data.data.later];
@@ -115,27 +99,47 @@ function getTVDBID(showName){
 	});
 }
 
-function isToBeShown(tvdbid){ 
+function presentHistory(data){ // Presents the history-data
+	var loc = $("#history");
+	var episodes = data.data; 
+	var addedEpisodes = new Array(); // Holds the added episodes, so we can check if a download is just a new version.
+	for (var i = 0; i < episodes.length; i++){
+		var ep = episodes[i];
+		if ( addedEpisodes.indexOf(ep.show_name+ep.season+ep.episode) != -1) {
+			continue; 
+		}
+		getStatus(ep); 
+		addedEpisodes.push(ep.show_name+ep.season+ep.episode);
+	}
+}
+
+function getStatus(episodeData){ 
 	// Tries to find out if download is backlog-download, or new episode.
 	show = true; 
 	$.ajax({
 		type: "GET", 
-		url: host + "/api/" + apiKey + "?cmd=show&tvdbid=" + tvdbid.toString(),
+		url: host + "/api/" + apiKey + "?cmd=show&tvdbid=" + episodeData.tvdbid.toString(),
 		data: String,
 		dataType: "jsonp", 
-		async: false,
-        cache: false,
-        timeout: 30000, 
         error: function(){
         	$("#log").append("Method isToBeShown failed for " + tvdbid.toString());
         	return true; 
         },
 		success: function(data){
-			console.log(data.data.status);
-			if (data.data.status != "Ended"){ // If it's not a running show, this episode is backlog-download.
-				return true;  
-			}
-			return false; 
+			checkStatus(data, episodeData);
 		}
 	});
+}
+
+function checkStatus(showData, episodeData){
+	if (showData.data.status != "Ended"){ // If it's not a running show, this episode is backlog-download.
+		addToView(episodeData)
+	}
+}
+
+function addToView(ep){
+	var loc = $("#history");
+	var btn = "<a href='"+"'<span class='glyphicon glyphicon-play'></span>"; // Needs padding.
+	loc.append( btn + "<a onClick='checkOff()' >"+ ep.show_name + " - " + ep.season + " x " + ep.episode +"</a><br>"); 
+	// loc.append(getFilePath(ep)); 
 }
